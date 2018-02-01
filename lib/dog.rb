2ca0c4f33cb593c615bc8a8117b40fdbaa5a9837
github username <github email address>
2018-01-31 23:18:01 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,41 @@
+require 'pry'
+
+class Dog
+  attr_accessor :name
+  @@all = []
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all.each do |d|
+      puts d.name
+    end
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+end
+
+# class Dog
+#    attr_accessor :name
+#
+#    @@all = []
+#
+#    def initialize(name)
+#      @name = name
+#      @@all << self
+#    end
+#
+#    def self.clear_all
+#     @@all.clear
+#    end
+#
+#    def self.all
+#      @@all.each do |d|
+#        puts d.name
+#      end
+#    end
+#  end
